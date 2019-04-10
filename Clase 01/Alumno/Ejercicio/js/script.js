e75c17e4030db7f58f2d3bde3b33100a922223c0
Fabricio <fabricio.rossi@gmail.com>
2019-04-09 21:46:30 -0300

    El primer ejercicio

---
+++ Clase 01/Alumno/Ejercicio/js/script.js
@@ -0,0 +1,24 @@
+// Parte 1
+
+var nombre = prompt('Como te llamas?')
+
+console.log('Hola ' + nombre)
+
+// Parte 2
+var numero = parseInt(20)
+
+var otroNumero = parseInt(prompt('Un numero para sumar?'))
+console.log('Resultado: ' + (otroNumero + numero))
+
+// Parte 3
+var numero = parseInt(20)
+
+var otroNumero = parseInt(prompt('Un numero para restar?'))
+console.log('Resultado: ' + (otroNumero - numero))
+
+// Parte 4
+var primerNumero = parseInt(prompt('Ingrese el primer numero:'))
+
+var segundoNumero = parseInt(prompt('Ingrese el primer numero:'))
+
+console.log('El Resultado es:' + primerNumero + segundoNumero)
---
+++ Clase 01/Alumno/Mini ejercicio/js/index.js
@@ -0,0 +1,5 @@
+var nombre
+
+nombre = prompt('Como te llamas?')
+
+console.log('Hola Como esta ' + nombre)
